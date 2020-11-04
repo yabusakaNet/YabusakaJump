91c72cb99a1088fd6b8cd53adb55e97c0df1d8af
ueda_a <qngph275@gmail.com>
2020-11-04 12:57:45 +0900

    背景ループ対応

--- Assets/Color Jump/Scripts/MoveBackground.cs
+++ Assets/Color Jump/Scripts/MoveBackground.cs
@@ -5,6 +5,7 @@ public class MoveBackground : MonoBehaviour
     public float offset;
     public float smoothTime = 0.5f;
     public float limitPosY;
+    public bool isLoop = false;
 
     bool isMove = false;
     Vector3 velocity = Vector3.zero;
@@ -19,8 +20,17 @@ public class MoveBackground : MonoBehaviour
         if (!isMove) {
             return;
         }
+
         var targetPos = new Vector3 (transform.position.x, transform.position.y + offset, transform.position.z);
         transform.position = Vector3.SmoothDamp (transform.position, targetPos, ref velocity, smoothTime);
+
+        if (transform.localPosition.y <= limitPosY) {
+            if (isLoop) {
+                transform.localPosition = Vector3.zero;
+            } else {
+                isMove = false;
+            }
+        }
     }
 
     public void StartMove ()
