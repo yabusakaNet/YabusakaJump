1c97b3b0b50c8c0d7fffeb3579bfab0fe9248cf8
Yamanaka Sho <ls279702@gmail.com>
2020-11-06 17:49:50 +0900

    スター時にBGMピッチ変更

---
+++ Assets/Color Jump/Scripts/BgmManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmManager : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioClip audioClipDead;
+    private AudioSource audioSource;
+
+    GameManager gameManager;
+    Player player;
+
+    void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        audioSource.Play ();
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        if (gameManager.isStar)
+        {
+            audioSource.pitch = 1.5f;
+        }
+        else
+        {
+            audioSource.pitch = 1f;
+        }
+    }
+}
